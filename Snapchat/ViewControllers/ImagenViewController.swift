86bbc866d0d5d35acbe827c0589443be561053fc
Mac Tecsup <mactecsup@MacBook-Air-de-Mac-2.local>
2018-06-07 19:44:52 -0500

    Tercer Commit Snapchatv2

--- Snapchat/ViewControllers/ImagenViewController.swift
+++ Snapchat/ViewControllers/ImagenViewController.swift
@@ -16,10 +16,12 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var chooseContactButton: UIButton!
     
     var imagePicker = UIImagePickerController()
+    var imagenID = NSUUID().uuidString
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        chooseContactButton.isEnabled = false
 
         // Do any additional setup after loading the view.
     }
@@ -28,11 +30,13 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = image
         imageView.backgroundColor = UIColor.clear
+        chooseContactButton.isEnabled = true
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
-        imagePicker.sourceType = .savedPhotosAlbum
+        //imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         present(imagePicker, animated: true, completion: nil)
     }
@@ -42,17 +46,33 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, U
         chooseContactButton.isEnabled = false
         let imagenesFolder = Storage.storage().reference().child("imagenes")
         let imagenData = UIImagePNGRepresentation(imageView.image!)!
+        let imagenURL = "\(imagenID).jpg"
         
-        imagenesFolder.child("\(NSUUID().uuidString).jpg").putData(imagenData, metadata: nil, completion: {(metadata, error) in
+        imagenesFolder.child(imagenURL).putData(imagenData, metadata: nil, completion: {(metadata, error) in
             print("Intentando subir la imagen ...")
             if error != nil {
                 print("Ocurrió un error:\(String(describing: error))")
+            }else{
+                print("Imagen subida correctamente")
+                let imagenRoute = imagenesFolder.child(imagenURL)
+                imagenRoute.downloadURL(completion: {(url, error) in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    if url != nil {
+                        self.performSegue(withIdentifier: "chooseContactSegue", sender: url!.absoluteString)
+                    }
+                })
             }
         })
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        let siguienteVC = segue.destination as! ChooseUserViewController
+        siguienteVC.imagenURL = sender as! String
+        siguienteVC.descrip = descriptionTextField.text!
+        siguienteVC.imagenID = imagenID
     }
 }
 
